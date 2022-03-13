7a976156354272c09d58b914945d962445379551
Opixelum <12w0zvkgazd3@opayq.net>
2022-03-13 19:04:47 +0100

    Added useState for wallet connection btn

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,23 @@
+import { useState } from "react"
+
 export default function Header() {
+    const [isConnected, setIsConnected] = useState(false)
+    const toggleWallet = () => {
+        if (isConnected) {
+            setIsConnected(false)
+        } else {
+            setIsConnected(true)
+        }
+    }
+
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
           <div className="container-fluid">
             <a className="navbar-brand" href="#">MyNFTWallet</a>
-            <button className="btn btn-outline-danger">Connect wallet</button>
+            <button
+              className="btn btn-outline-danger"
+              onClick={toggleWallet}
+            >{isConnected ? "Disconnect wallet" : "Connect wallet"}</button>
           </div>
         </nav>
     )
